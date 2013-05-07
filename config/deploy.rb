70b0323260861ec9ef94d22241680e6cae8f9842
Hanes <vykyja@seznam.cz>
2013-05-07 16:23:23 +0200

    svatba capistrano

---
+++ config/deploy.rb
@@ -0,0 +1,45 @@
+set :user, 'root'
+set :domain, '37.157.198.135'
+
+set :scm, :git
+
+#set :repository, "gitolite@37.157.198.135:farnost"
+#set :repository, "file:/home/gitolite/repositories/farnost.git"
+
+set :application, "svatba"
+set :repository, "gitbox:svatba"
+set :deploy_to, "/u/apps/#{application}"
+set :deploy_via, :remote_cache
+
+
+set :copy_cache, true
+
+set :use_sudo, false
+
+set :scm, :git # Should I change this to :none
+set :branch, "master"
+
+# roles (servers)
+role :web, domain
+role :app, domain
+role :db,  domain, :primary => true
+
+# deploy config
+
+
+
+# If you are using Passenger mod_rails uncomment this:
+
+
+namespace :assets do
+  desc "Compile assets"
+  task :precompile, :roles => :app do
+    run "cd #{release_path} && rake RAILS_ENV=#{rails_env} assets:precompile"
+  end
+end
+
+after "deploy:symlink" do
+  run "chmod -R 0666 #{current_path}/log"
+  run "chown -R www-data:www-data #{current_path}/"
+end
+
