52fa20fa368148bca0d594a808ac994769012d36
Hanes <vykyja@seznam.cz>
2013-05-07 16:17:24 +0200

    svatba kopie

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Svatba3::Application.config.secret_token = '900ec2af6da8178104a506a6c489a4d1c09e9c76917b2568d0dd0df6c2ade45c217204e08dab55da8da457285a63deb43396ce5e1d79c9a9221af1295a195d46'
