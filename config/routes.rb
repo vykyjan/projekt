50c1a138e2a3f00337f322a01f210f69230ebdb1
Hanes <vykyja@seznam.cz>
2013-05-07 17:52:51 +0200

    add user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Svatba3::Application.routes.draw do
+  get "users/new"
+
   get "home/index"
  match '/home',    to: 'home#home'
   # The priority is based upon order of creation:
@@ -50,7 +52,7 @@ Svatba3::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
