fb6a36fd7c0cf8ce5f288351d3324cb737b60fc0
Hanes <vykyja@seznam.cz>
2013-05-08 16:07:14 +0200

    javasc1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,6 +16,7 @@ GalleryJqueryFileUpload::Application.configure do
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = true
+config.assets.precompile += %w( *.js )
 
   # Generate digests for assets URLs
   config.assets.digest = true
