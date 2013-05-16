c1eea8e5adf4d1275c7658f9d402c9822d6608f7
Hanes <vykyja@seznam.cz>
2013-05-16 18:41:49 +0200

    nejde4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,11 +12,11 @@ GalleryJqueryFileUpload::Application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
-  config.assets.compress = true
+  config.assets.compress = false
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
-
+  config.assets.compile = true
+  config.assets.precompile += %w( *.js )
   # Generate digests for assets URLs
   config.assets.digest = true
 
