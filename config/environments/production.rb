2e156bf8182b683a7d2614ab7141ccb5b5e85cd7
Jan Vykydal <vykyjan@gmail.com>
2013-12-08 15:47:27 +0100

    javascript deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,10 +26,10 @@ GalleryJqueryFileUpload::Application.configure do
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
-  config.assets.compress = true
+
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
