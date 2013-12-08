95558413415f09acc245f3030635a2bef5182aff
Jan Vykydal <vykyjan@gmail.com>
2013-12-08 16:35:43 +0100

    javascript deploy3

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,6 @@ module GalleryJqueryFileUpload
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    config.assets.precompile += %w( 'application.js', 'application.css', )
+
   end
 end
