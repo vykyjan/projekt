4e01f026623df62a11febfea782a638f7b05db64
Jan Vykydal <vykyjan@gmail.com>
2013-12-09 12:23:30 +0100

    add galleries7

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,7 @@ module GalleryJqueryFileUpload
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.enabled = true
+    config.assets.precompile += %w( 'application.js', 'application.css')
 
   end
 end
