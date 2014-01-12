e835b40a038e5d00b830b7f8428febc685392e67
Jan Vykydal <vykyjan@gmail.com>
2014-01-12 19:58:55 +0100

    bootstrap dropmenu

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-
+//= require bootstrap
 //= require jquery
 //= require jquery_ujs
 
@@ -32,7 +32,7 @@
 
 //= require slimbox2
 
-//= require bootstrap
+
 //= require bootstrap-image-gallery.min
 
 //= require twitter/bootstrap
